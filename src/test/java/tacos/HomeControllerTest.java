8138dcbc741567f113bab43893ed06a687356c9b
Biaoge <biaoge@evestemptation.com>
2025-11-06 23:32:47 -0500

    Fix the failed HomeControllerTest so that build with mvnw could pass

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -2,8 +2,18 @@ package tacos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+
+import tacos.data.jpa.IngredientRepositoryJPA;
+import tacos.data.jpa.OrderRepositoryJPA;
+import tacos.data.jpa.TacoRepositoryJPA;
+import tacos.data.jpa.UserRepositoryJPA;
+import tacos.web.OrderProps;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -11,11 +21,33 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest(/*HomeController.class*/)
+@WebMvcTest
+@AutoConfigureMockMvc(addFilters = false)
 public class HomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private IngredientRepositoryJPA ingredientRepository;
+
+    @MockBean
+    private TacoRepositoryJPA tacoRepository;
+
+    @MockBean
+    private OrderRepositoryJPA orderRepository;
+
+    @MockBean
+    private UserRepositoryJPA userRepository;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @MockBean
+    private OrderProps orderProps;
+
+    @MockBean
+    private EntityLinks entityLinks;
 
     @Test
     public void testHomePage() throws Exception {
